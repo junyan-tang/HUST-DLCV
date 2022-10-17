d3b19090151dea47b2dc1728c12808e388a84d3f
junyan-tang <106074923+junyan-tang@users.noreply.github.com>
2022-10-17 16:42:47 +0800

    Add files via upload

---
+++ lab02/linearly interpolating.py
@@ -0,0 +1,8 @@
+z = torch.randn(2, latent_dim)
+a = torch.FloatTensor(latent_dim, 10)
+for i in range(latent_dim):
+    a[i] = torch.linspace(z[0][i], z[1][i], 10)
+
+b = a.t()
+gen_images = generator(b)
+torchvision.utils.save_image(gen_images.data[:], "images_trans.png", normalize=True)
